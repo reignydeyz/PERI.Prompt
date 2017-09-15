5374a2b8603db6e1235d09785704414842015269
Alvin <alvin.chua@ghl.com>
2017-09-15 12:21:32 +0800

    Added CategoryBlogs

---
+++ PERI.Prompt.BLL/CategoryBlogs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PERI.Prompt.BLL
+{
+    /// <summary>
+    /// This simplifies the Category class
+    /// </summary>
+    public class CategoryBlogs
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public int BlogCount { get; set; }
+    }
+}
