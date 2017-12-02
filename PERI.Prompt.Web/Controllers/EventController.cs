4e479d23c3204cb8baeb69d3abdf6595c2780da0
Alvin Chua <actchua@periapsys.com>
2017-12-02 09:48:56 +0800

    Added EventController

---
+++ PERI.Prompt.Web/Controllers/EventController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PERI.Prompt.Web.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> View(int id)
+        {
+            var context = new EF.SampleDbContext();
+
+            var rec = await new BLL.Event(context).Get(new EF.Event { EventId = id });
+
+            return View(rec);
+        }
+    }
+}
