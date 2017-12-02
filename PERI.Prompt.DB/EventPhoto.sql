90dcabf3bc96bd0335d0d4f7feeb996519f5a7fc
Alvin Chua <actchua@periapsys.com>
2017-12-02 09:39:05 +0800

    Added EventPhoto script

---
+++ PERI.Prompt.DB/EventPhoto.sql
@@ -0,0 +1,13 @@
+﻿create table EventPhoto
+(
+	EventId int not null,
+	PhotoId int not null,
+
+	primary key(EventId, PhotoId),	
+
+	constraint UQ_EventPhoto_EventId_PhotoId unique (EventId, PhotoId),
+	constraint FK_EventPhoto_Event foreign key (EventId)
+		references [Event] (EventId) on delete cascade,
+	constraint FK_EventPhoto_Photo foreign key (PhotoId)
+		references [Photo] (PhotoId) on delete cascade
+)
