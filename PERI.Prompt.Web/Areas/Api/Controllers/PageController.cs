bbd45e5f339bf9c01e0746866192f960bb435fdf
VAN\Alvin <actchua@periapsys.com>
2018-11-06 21:41:32 +0800

    PageController

---
+++ PERI.Prompt.Web/Areas/Api/Controllers/PageController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PERI.Prompt.Web.Areas.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Page")]
+    public class PageController : Controller
+    {
+    }
+}
