12df5d5b33da718926a8b861844d44a0f3e48128
VAN\Alvin <actchua@periapsys.com>
2018-11-08 22:04:55 +0800

    API MenuController

---
+++ PERI.Prompt.Web/Areas/Api/Controllers/MenuController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PERI.Prompt.BLL;
+
+namespace PERI.Prompt.Web.Areas.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Menu")]
+    public class MenuController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public MenuController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> All()
+        {
+            var res = from r in (await new BLL.Menu(unitOfWork).Find(new EF.Menu()))
+                      select new
+                      {
+                          r.MenuId,
+                          r.Name,
+                          MenuItems = r.MenuItem.Count()
+                      };
+
+            return Json(res);
+        }
+
+        [HttpGet("[action]")]
+        [Route("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var res = await new BLL.Menu(unitOfWork).Get(new EF.Menu { MenuId = id });
+
+            return Json(from r in res.MenuItem
+                        select new {
+                            r.MenuItemId,
+                            r.Label,
+                            r.Url
+                        });
+        }
+    }
+}
