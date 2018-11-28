2a021a162b745e855e12f3bba8660432281f84ce
Alvin <infinite.jerk@gmail.com>
2018-11-28 15:49:29 +0800

    SectionController API

---
+++ PERI.Prompt.Web/Areas/Api/Controllers/SectionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PERI.Prompt.BLL;
+
+namespace PERI.Prompt.Web.Areas.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Section")]
+    public class SectionController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public SectionController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+    }
+}
