b0609dd98b3dbb4e9c216b9af985aae65f1d8b65
reignydeyz <infinite.jerk@gmail.com>
2018-11-28 21:00:04 +0800

    SectionController API

--- PERI.Prompt.Web/Areas/Api/Controllers/SectionController.cs
+++ PERI.Prompt.Web/Areas/Api/Controllers/SectionController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Cors;
@@ -19,6 +20,44 @@ namespace PERI.Prompt.Web.Areas.Api.Controllers
         public SectionController(IUnitOfWork unitOfWork)
         {
             this.unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Route("All")]
+        public async Task<IActionResult> GetSections()
+        {
+            var template = (await new BLL.Template(unitOfWork).Find(new EF.Template())).Where(x => x.DateInactive == null).First();
+
+            var obj = from r in (await new BLL.Section(unitOfWork).Find(new EF.Section { TemplateId = template.TemplateId }))
+                      select new
+                      {
+                          r.SectionId,
+                          r.Name
+                      };
+                        
+            return Json(obj);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var obj = await new BLL.Section(unitOfWork).Get(new EF.Section { SectionId = id });
+                        
+            dynamic obj1 = new ExpandoObject();
+            obj1.sectionId = obj.SectionId;
+            obj1.name = obj.Name;
+            obj1.items = from r in obj.SectionItem
+                         select new
+                         {
+                             r.Title,
+                             r.Body,
+                             Photo = r.SectionItemPhoto.FirstOrDefault() == null ? "" : r.SectionItemPhoto.First().Photo.Url,
+                             Properties = r.SectionItemProperty.ToList()
+                         };
+            
+            return Json(obj1);
+            
         }
     }
 }
